b6a3b6959b374f3734fc4a00e31f6438908376b8
Lauren <lauren.eastridge@formidablelabs.com>
2015-07-28 14:53:12 -0700

    initial commit

---
+++ demo/app.jsx
@@ -0,0 +1,17 @@
+/*global document:false*/
+import React from "react";
+import {BoilerplateComponent} from "../src/index";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="demo">
+        < BoilerplateComponent />
+      </div>
+    );
+  }
+}
+
+const content = document.getElementById("content");
+
+React.render(<App/>, content);
---
+++ src/components/boilerplate-component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class BoilerplateComponent extends React.Component {
+  render() {
+    return <div>Edit me!</div>;
+  }
+}
