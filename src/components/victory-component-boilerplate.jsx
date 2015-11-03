d871f9c73fcb2b53033a4e9c218304c8bdd0d64f
Lauren <lauren.eastridge@formidablelabs.com>
2015-11-02 16:31:38 -0800

    no radium

--- src/components/victory-component-boilerplate.jsx
+++ src/components/victory-component-boilerplate.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Radium from "radium";
 
-@Radium
 export default class VictoryComponentBoilerplate extends React.Component {
   render() {
     return <div>Edit me!</div>;
