06eb1c09bdf7493e155d225e5bb903b7c06e5a21
Brian Beck <exogen@gmail.com>
2015-11-04 15:38:16 -0800

    Provide own .babelrc, add Radium
    This adds a .babelrc until we can resolve
    FormidableLabs/builder-react-component#4. Since stage 0 is now being
    used as intended, bring back `@Radium`.
    
    /cc @ryan-roemer @boygirl

--- src/components/victory-component-boilerplate.jsx
+++ src/components/victory-component-boilerplate.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Radium from "radium";
 
+@Radium
 export default class VictoryComponentBoilerplate extends React.Component {
   render() {
     return <div>Edit me!</div>;
