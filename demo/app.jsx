5249224d47876393d7eca66eadba5b6851349005
Per Nilsson <per.nilsson@formidablelabs.com>
2016-01-18 09:14:00 -0800

    Split up App and Demo to fix hot reloading
    * Remove webpack-dev-server from index.html; webpack runs with --inline
    in hot mode, which removes the need for an external script.
    * Fixes https://github.com/FormidableLabs/victory/issues/165

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,18 +1,8 @@
 /*global document:false*/
 import React from "react";
 import ReactDOM from "react-dom";
-import {VictoryComponentBoilerplate} from "../src/index";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="demo">
-        <VictoryComponentBoilerplate />
-      </div>
-    );
-  }
-}
+import Demo from "./demo";
 
 const content = document.getElementById("content");
 
-ReactDOM.render(<App/>, content);
+ReactDOM.render(<Demo/>, content);
